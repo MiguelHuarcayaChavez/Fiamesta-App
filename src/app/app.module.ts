0d8cfad175f75760e9fa64256a03d8ee76e827e4
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-05-27 17:16:14 -0500

    create componentnt for auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAdminComponent } from './auth/components/login-admin/login-admin.component';
+import { LoginCustomerComponent } from './auth/components/login-customer/login-customer.component';
+import { RegisterComponent } from './auth/components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAdminComponent,
+    LoginCustomerComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
