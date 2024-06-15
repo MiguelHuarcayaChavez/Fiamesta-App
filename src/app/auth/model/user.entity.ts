d09e87b8ecdcd4645d98cc560e237033dd23aee2
Miguel Huarcaya Chavez <u202116207@upc.edu.pe>
2024-06-15 01:55:07 -0500

    feat: public- profile data

--- src/app/auth/model/user.entity.ts
+++ src/app/auth/model/user.entity.ts
@@ -1,9 +1,11 @@
 export class UserEntity {
+  id: string;
   username: string;
   password: string;
   dni: string;
   admin: boolean | undefined;
   constructor() {
+    this.id='';
     this.username='';
     this.password='';
     this.dni='';
--- src/app/public/components/profile-data/profile-data.component.ts
+++ src/app/public/components/profile-data/profile-data.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {UserEntity} from "../../../auth/model/user.entity";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../../auth/services/auth.service";
 
 @Component({
   selector: 'app-profile-data',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile-data.component.css'
 })
 export class ProfileDataComponent {
+  user : UserEntity = {} as UserEntity;
 
+  constructor(private route: ActivatedRoute, private router: Router,private api: AuthService) {
+    this.user.dni = this.route.snapshot.params['dni'];
+  }
+
+  ngOnInit(){
+    console.log(this.user.dni)
+    this.api.findUserByDni(this.user.dni).subscribe((data:any)=>{
+      this.user.username = data[0].username;
+      this.user.dni = data[0].dni;
+      this.user.id = data[0].id;
+    })
+  }
 }
